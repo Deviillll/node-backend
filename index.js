7f22eb140e960a62e3e13058ce0ec46a3076d33f
Hammad Hussain <93250@gcslahore.edu.pk>
2024-09-20 22:46:20 +0500

    add env

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
